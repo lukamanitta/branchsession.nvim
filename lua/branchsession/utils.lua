4ca8fa05dee7fa8c7db39b2539f4fdd7a4a3a48e
lukamanitta <lukablueowl51@gmail.com>
2023-01-14 18:34:57 +1000

    refactor: collapse if-statement into simple comparison

--- lua/branchsession/utils.lua
+++ lua/branchsession/utils.lua
@@ -6,11 +6,7 @@ end
 
 function U.inside_git_dir()
     local _ = U.shell("git rev-parse --is-inside-git-dir")
-    if vim.v.shell_error == 0 then
-        return true
-    else
-        return false
-    end
+    return vim.v.shell_error == 0
 end
 
 return U
